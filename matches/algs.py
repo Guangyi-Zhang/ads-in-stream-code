8f1ccacdc9882dcaa66123009af674b2f35ef079
Guangyi <guangyi.z@outlook.com>
2024-09-28 20:14:07 +0800

    match_by_forward_greedy

--- matches/algs.py
+++ matches/algs.py
@@ -1,6 +1,31 @@
 import numpy as np
 import networkx as nx
 import math
+from operator import itemgetter
+
+
+##### Utilities #####
+def is_sorted(l):  
+    return all(l[i] <= l[i+1] for i in range(len(l) - 1))
+
+
+##### Algorithms #####
+def match_by_forward_greedy(G):
+    V1 = [n for n, d in G.nodes(data=True) if d["bipartite"] == 0]
+    V2 = [n for n, d in G.nodes(data=True) if d["bipartite"] == 1]
+    assert is_sorted(V2)
+
+    M = []
+    i_taken = set()
+    for j in V2:
+        iw = [(i, d['weight']) for i, _, d in G.in_edges(nbunch=j, data=True) if i not in i_taken]
+        if len(iw) == 0:
+            continue
+        i_star, w_star = min(iw, key=itemgetter(1))
+        i_taken.add(i_star)
+        M.append((i_star, j))
+    
+    return M
 
 
 def match_by_flow(G, q):
@@ -26,10 +51,10 @@ def match_by_flow(G, q):
     flowDict = nx.max_flow_min_cost(G, 's', 't')
     # print(f"mincost = {nx.cost_of_flow(G, flowDict)}")
 
-    M = set()
+    M = []
     A = [v for v, flow in flowDict['s'].items() if flow == 1]
     for i in A:
         j = [v for v, flow in flowDict[i].items() if flow == 1][0]
-        M.add((i,j))
+        M.append((i,j))
 
     return M
--- tests/test_all.py
+++ tests/test_all.py
@@ -2,7 +2,7 @@ import pytest
 import networkx as nx
 
 from matches.bip import create_random_weighted_dibipartite
-from matches.algs import match_by_flow
+from matches.algs import match_by_flow, match_by_forward_greedy
 
 
 @pytest.fixture
@@ -24,6 +24,44 @@ def G_1vs2():
     return G
 
 
+@pytest.fixture
+def G_end_big():
+    n1 = 10
+    n2 = 10
+
+    top_nodes = set(range(n1))
+    bottom_nodes = set(range(n1, n1 + n2))
+
+    G = nx.DiGraph()
+    G.add_nodes_from(top_nodes, bipartite=0)
+    G.add_nodes_from(bottom_nodes, bipartite=1)
+    
+    G.add_edge(0, 10, weight=-2, capacity=1)
+    G.add_edge(1, 11, weight=-2, capacity=1)
+    G.add_edge(0, 11, weight=-3, capacity=1)
+
+    G.add_edge(2, 12, weight=-1, capacity=1)
+    G.add_edge(3, 13, weight=-1, capacity=1)
+    G.add_edge(4, 14, weight=-1, capacity=1)
+    G.add_edge(5, 15, weight=-1, capacity=1)
+    G.add_edge(6, 16, weight=-1, capacity=1)
+    G.add_edge(7, 17, weight=-1, capacity=1)
+    #G.add_edge(8, 18, weight=-1, capacity=1)
+
+    G.add_edge(0, 19, weight=-100, capacity=1)
+
+    return G
+
+
+def test_forward_greedy(G_end_big):
+    M = match_by_forward_greedy(G_end_big)
+    R = 0
+    for e in M:
+        R = R + G_end_big.edges[e]['weight']
+
+    assert R == -4 + -6
+
+
 def test_flow(G_1vs2):
     M = match_by_flow(G_1vs2, 0.1)
     R = 0
@@ -31,3 +69,4 @@ def test_flow(G_1vs2):
         R = R + G_1vs2.edges[e]['weight']
 
     assert R == -4
+
