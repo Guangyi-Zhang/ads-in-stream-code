28f0f4b1b20d4c58b1b7ba705dc66fd72fc180ba
Guangyi <guangyi.z@outlook.com>
2024-10-09 13:26:25 +0800

    minor

--- run-real.sh
+++ run-real.sh
@@ -5,22 +5,14 @@ ver=-5 # test
 ver=5001 # @20241009: test
 ver=5002 # @20241009: 
 ver=5003 # @20241009: 
+ver=5004 # @20241009: 
 
-rep=1
+rep=2
 seed=$(date +%s)
 
 q=0.1
 
-for data in criteo ; do
-    #for method in "flow" "flowgreedy" "mwm" "forwardgreedy" "greedy" "onlinegreedy" "backwardgreedy" "backwardgreedyproxy"; do
-    for method in "flowgreedy" "mwm" "forwardgreedy" "greedy" "onlinegreedy" "backwardgreedy" "backwardgreedyproxy"; do
-        python ex.py -v $ver -r $rep -s $seed -m $method -q $q -d "realData-$data" 
-        #nohup ./build/main/Main -b $buildtype -v $ver -r $rep -s $seed -d random_graph_wTrue_nghb10_n${n}.csv -k 10 -t "note" -m $method  &
-    done
-done
-
-
-for data in yt ; do
+for data in criteo yt; do
     for method in "flow" "flowgreedy" "mwm" "forwardgreedy" "greedy" "onlinegreedy" "backwardgreedy" "backwardgreedyproxy"; do
         python ex.py -v $ver -r $rep -s $seed -m $method -q $q -d "realData-$data" 
         #nohup ./build/main/Main -b $buildtype -v $ver -r $rep -s $seed -d random_graph_wTrue_nghb10_n${n}.csv -k 10 -t "note" -m $method  &
